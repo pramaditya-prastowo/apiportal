e2f6aab2f526f65e9a91d5b286810cba6b91f5b3
pramadityo.prastowo <pramadityo.prastowo@gmail.com>
2022-10-24 13:05:22 +0700

    init project api portal

--- src/main/java/mii/bsi/apiportal/ApiportalApplication.java
+++ src/main/java/mii/bsi/apiportal/ApiportalApplication.java
@@ -2,6 +2,9 @@ package mii.bsi.apiportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiportalApplication {
@@ -10,4 +13,8 @@ public class ApiportalApplication {
 		SpringApplication.run(ApiportalApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
