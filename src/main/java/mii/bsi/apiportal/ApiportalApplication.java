83d74a21b91ed793898bb2d6b3248bec11ecfbf8
pramadityo.prastowo <pramadityo.prastowo@gmail.com>
2022-10-24 09:31:37 +0700

    Initial commit

---
+++ src/main/java/mii/bsi/apiportal/ApiportalApplication.java
@@ -0,0 +1,13 @@
+package mii.bsi.apiportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mii/bsi/apiportal/ApiportalApplicationTests.java
@@ -0,0 +1,13 @@
+package mii.bsi.apiportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
