ade9f733d6287b9f62e1e9a2042606557aaac1df
pramadityo.prastowo <pramadityo.prastowo@gmail.com>
2022-11-28 14:15:07 +0700

    de

--- src/main/java/mii/bsi/apiportal/repository/ServiceApiRepository.java
+++
@@ -1,9 +0,0 @@
-package mii.bsi.apiportal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mii.bsi.apiportal.domain.ServiceApi;
-
-public interface ServiceApiRepository extends JpaRepository<ServiceApi, String> {
-
-}
